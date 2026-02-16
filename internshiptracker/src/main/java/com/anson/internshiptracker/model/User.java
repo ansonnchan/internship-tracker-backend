b2c5a86967e49a7ea25cbc41f38d1a09de41272d
Anson Chan <100487472+ansonnchan@users.noreply.github.com>
2026-02-15 20:15:10 -0800

    change #2

--- internshiptracker/src/main/java/com/anson/internshiptracker/model/User.java
+++ internshiptracker/src/main/java/com/anson/internshiptracker/model/User.java
@@ -1,20 +1,38 @@
 package com.anson.internshiptracker.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @Data
 public class User {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank(message = "Name is required")
     private String name;
-
-    @Column(unique = true)
+    
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
-
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    @JsonIgnore // Prevent password from being serialized in JSON responses
     private String password;
+    
+    // One user can have many applications
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore 
+    private List<Application> applications = new ArrayList<>();
 }
